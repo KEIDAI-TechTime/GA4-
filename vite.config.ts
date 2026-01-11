491ede56621a02a7b3628f3bc211fa366d3b3b10
KEIDAI-TechTime <kdm@techtime-link.com>
2026-01-12 00:32:04 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 import AutoImport from "unplugin-auto-import/vite";
 
-const base = process.env.BASE_PATH || "/";
-const isPreview = process.env.IS_PREVIEW ? true : false;
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    __BASE_PATH__: JSON.stringify(base),
-    __IS_PREVIEW__: JSON.stringify(isPreview),
-    __READDY_PROJECT_ID__: JSON.stringify(process.env.PROJECT_ID || ""),
-    __READDY_VERSION_ID__: JSON.stringify(process.env.VERSION_ID || ""),
-    __READDY_AI_DOMAIN__: JSON.stringify(process.env.READDY_AI_DOMAIN || ""),
-  },
   plugins: [
     react(),
     AutoImport({
@@ -58,7 +49,6 @@ export default defineConfig({
             "Outlet",
           ],
         },
-        // React i18n
         {
           "react-i18next": ["useTranslation", "Trans"],
         },
@@ -66,10 +56,9 @@ export default defineConfig({
       dts: true,
     }),
   ],
-  base,
   build: {
     sourcemap: true,
-    outDir: "out",
+    outDir: "dist",
   },
   resolve: {
     alias: {
